54c98800504c8049cba91d58ad22575b1bd3671b
pranay823 <102896608+pranay823@users.noreply.github.com>
2024-01-09 23:57:27 +0530

    make sign in and sign out and remove bugs

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,25 @@
-import React from 'react'
+
 import Login from './Login'
 import Browse from './Browse'
 import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom'
 
+
+
 const Body = () => {
-    const appRouter = createBrowserRouter([
+
+       const appRouter = createBrowserRouter([
         {
           path :"/",
-          element : <Login/>
+          element : <Login />
         },
         {
          path : "/browse",
-         element : <Browse />
+         element : <Browse  />
         }
       ])
     
+
   return (
     <div>
  
